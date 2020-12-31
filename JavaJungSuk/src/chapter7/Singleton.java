0822295213de5950e2717ae416af7b905ff50156
JunHo <u_innovation@naver.com>
2020-12-31 23:23:32 +0900

    Singleton

---
+++ JavaJungSuk/src/chapter7/Singleton.java
@@ -0,0 +1,23 @@
+package chapter7;
+
+final class Singleton {
+	private static Singleton s;
+
+	private Singleton() {
+	}
+
+	public static Singleton getInstance() {
+		if (s == null) {
+			s = new Singleton();
+		}
+		return s;
+	}
+
+}
+
+class SingletonTest {
+	public static void main(String[] args) {
+//		Singleton s = new Singleton();
+		Singleton a = Singleton.getInstance();
+	}
+}
