0c97c7bfeda9ae6f0d312468764f3ec56eeb867c
JunHo <u_innovation@naver.com>
2021-01-02 12:23:53 +0900

    Grouping_abstract

---
+++ JavaJungSuk/src/chapter7/Marine.java
@@ -0,0 +1,33 @@
+package chapter7;
+
+abstract class Unit {
+	int x, y;
+	abstract void move(int x, int y);
+	void stop() { /*해당 위치에 정지*/}
+}
+
+public class Marine extends Unit{
+//	int x, y;
+//	void move(int x, int y) {}
+//	void stop() {}
+	void move(int x, int y) { /* 지정된 위치로 이동*/}
+	void stimPack() {}
+}
+
+class Tank extends Unit{
+//	int x, y;
+//	void move(int x, int y) {}
+//	void stop() {}
+	void move(int x, int y) {}
+	void changeMode() {}
+}
+
+class Dropship extends Unit{
+//	int x, y;
+//	void move(int x, int y) {}
+//	void stop() {}
+	void move(int x, int y) {}
+	void load() {}
+	void unload() {}
+}
+
