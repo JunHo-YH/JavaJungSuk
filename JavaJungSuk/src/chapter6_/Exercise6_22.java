01a217027fbe3682ca2db70d86215a54fd4e4411
JunHo <u_innovation@naver.com>
2021-01-17 21:36:13 +0900

    연습

---
+++ JavaJungSuk/src/chapter6_/Exercise6_22.java
@@ -0,0 +1,23 @@
+package chapter6_;
+
+public class Exercise6_22 {
+
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+
+		String s = "a";
+		System.out.println(s.charAt(0));
+		
+		
+		
+		
+	}
+
+}
