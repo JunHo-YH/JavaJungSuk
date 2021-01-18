a219a2a5d5eb8ad204b2364be0a2b0ce517c7f4c
JunHo <u_innovation@naver.com>
2021-01-18 22:49:27 +0900

    자바의정석 6장 객체지향

--- JavaJungSuk/src/chapter6_/Exercise6_22.java
+++ JavaJungSuk/src/chapter6_/Exercise6_22.java
@@ -2,21 +2,18 @@ package chapter6_;
 
 public class Exercise6_22 {
 
-	
-	
-	
-	
-	
-	
-	
-	
-	public static void main(String[] args) {
-
-		String s = "a";
-		System.out.println(s.charAt(0));
+	public static boolean isNumber(String str) {
+		if (str == null || str.equals("")) {
+			return false;
+		}
 		
-		
-		
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if (ch < '0' || ch > '9') {
+				return false;
+			}
+		}
+		return true;
 		
 	}
 
--- JavaJungSuk/src/chapter6_/Test.java
+++ JavaJungSuk/src/chapter6_/Test.java
@@ -1,18 +1,6 @@
 package chapter6_;
 
 public class Test {
-	int iv = 10;
-	static int cv = 20;
-
-	int iv2 = cv;
-
 	public static void main(String[] args) {
-		Test t = new Test();
-		Test t2 = new Test();
-
-		t.cv = t.iv;
-		System.out.println(t.cv);
-		System.out.println(t2.cv);
 	}
-
 }
